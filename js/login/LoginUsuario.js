155fa924a840a83594ff07ff1b385e627b235261
Marcelo Ferreira <joamarcelo@gmail.com>
2017-08-04 10:06:26 -0300

    Maps cards' list to user

--- js/login/LoginUsuario.js
+++ js/login/LoginUsuario.js
@@ -1,16 +1,21 @@
 let logado = !!localStorage.getItem("logado");
+let usuario = localStorage.getItem("nomeUsuario");
+let login = new EventEmitter2();
 
 LoginUsuario_render({
     logado,
-    usuario: localStorage.getItem("nomeUsuario"),
+    usuario,
     onLogin: (nomeUsuario) => {
         logado = true;
         localStorage.setItem("logado", true);
         localStorage.setItem("nomeUsuario", nomeUsuario);
+        usuario = nomeUsuario;
+        login.emit("login");
     },
     onLogout: () => {
         logado = false;
         localStorage.removeItem("logado");
         localStorage.removeItem("nomeUsuario");
+        login.emit("logout");
     },
 });
--- js/mural/Mural.js
+++ js/mural/Mural.js
@@ -1,7 +1,6 @@
 const Mural = (function(_render, Filtro){
     "use strict"
     let cartoes = getCards();
-    cartoes.forEach(card => preparaCartao(card));
 
     const render = () => _render({cartoes: cartoes, filtro: Filtro.tagsETexto});
 
@@ -10,12 +9,13 @@ const Mural = (function(_render, Filtro){
     Filtro.on("filtrado", render)
 
     function getCards() {
-        return (JSON.parse(localStorage.getItem("cartoes")) || [])
-            .map(card => new Cartao(card.conteudo, card.tipo));
+        return (JSON.parse(localStorage.getItem(`cartoes[${usuario}]`)) || [])
+            .map(card => new Cartao(card.conteudo, card.tipo))
+            .map(card => preparaCartao(card));
     }
 
     function putCartds() {
-        localStorage.setItem("cartoes", JSON.stringify(cartoes.map(card => ({conteudo: card.conteudo, tipo: card.tipo}))));
+        localStorage.setItem(`cartoes[${usuario}]`, JSON.stringify(cartoes.map(card => ({conteudo: card.conteudo, tipo: card.tipo}))));
     }
 
     function preparaCartao(cartao) {
@@ -29,6 +29,7 @@ const Mural = (function(_render, Filtro){
                 putCartds();
                 render();
             });
+            return cartao;
     }
 
     function adiciona(cartao){
@@ -42,6 +43,16 @@ const Mural = (function(_render, Filtro){
         alert("Você não está logado");
         return false;
     }
+
+    login.on("login", () => {
+        cartoes = getCards();
+        render();
+    });
+
+    login.on("logout", () => {
+        cartoes = [];
+        render();
+    });
 
     return Object.seal({
         adiciona
