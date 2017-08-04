3c923845cadef75d891a52b60bbb2562540cfd12
Marcelo Ferreira <joamarcelo@gmail.com>
2017-08-04 09:27:33 -0300

    Saves cards on localStorage

--- js/mural/Mural.js
+++ js/mural/Mural.js
@@ -1,19 +1,41 @@
 const Mural = (function(_render, Filtro){
     "use strict"
-    let cartoes = []
+    let cartoes = getCards();
+    cartoes.forEach(card => preparaCartao(card));
+
     const render = () => _render({cartoes: cartoes, filtro: Filtro.tagsETexto});
 
+    render();
+
     Filtro.on("filtrado", render)
+
+    function getCards() {
+        return (JSON.parse(localStorage.getItem("cartoes")) || [])
+            .map(card => new Cartao(card.conteudo, card.tipo));
+    }
+
+    function putCartds() {
+        localStorage.setItem("cartoes", JSON.stringify(cartoes.map(card => ({conteudo: card.conteudo, tipo: card.tipo}))));
+    }
+
+    function preparaCartao(cartao) {
+            cartao.on("mudanca.**", () => {
+                putCartds();
+                render();
+            });
+            cartao.on("remocao", () => {
+                cartoes = cartoes.slice(0);
+                cartoes.splice(cartoes.indexOf(cartao), 1);
+                putCartds();
+                render();
+            });
+    }
 
     function adiciona(cartao){
         if (logado) {
             cartoes.push(cartao)
-            cartao.on("mudanca.**", render)
-            cartao.on("remocao", ()=>{
-                cartoes = cartoes.slice(0)
-                cartoes.splice(cartoes.indexOf(cartao),1)
-                render()
-            })
+            putCartds();
+            preparaCartao(cartao);
             render()
             return true
         }
